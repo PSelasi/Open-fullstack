eb958056d371c96205888ae74c0f547060858a8a
Selasi Prince <72169628+PSelasi@users.noreply.github.com>
2026-02-26 20:05:19 +0000

    Add files via upload

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,56 @@
+function Header({course}) {
+ return(
+  <>
+    <h1>{course}</h1>
+  </>
+ )
+};
+
+function Content({parts }) {
+ return(
+  <>
+  <Part parts = {parts[0]}/>
+  <Part parts = {parts[1]}/>
+  <Part parts = {parts[2]}/>
+  </>
+ )
+};
+
+function Total({parts}) {
+  return( 
+  <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+)
+};
+
+const Part =({parts}) =>{
+return (
+  <>
+  <p>
+    {parts.name} {parts.exercises}  
+  </p>
+  </>
+)
+};
+
+const App = () => {
+  const course = 'Half Stack application development' 
+  const parts = [
+    {name:'Fundamentals of React',
+    exercises: 10},
+    {name: 'Using props to pass data',
+    exercises: 7},
+    {name: 'State of a component',
+    exercises: 14}
+  ]  
+  
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content parts = {parts}/>
+      <Total parts = {parts}/>
+    </div>
+  )
+}
+
+export default App
